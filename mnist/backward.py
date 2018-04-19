4c168184e25c83729d4c88e7e07fefbe01795d84
ZALZ <824266572@qq.com>
2018-04-19 10:25:11 +0800

    Update backward.py

--- mnist/backward.py
+++ mnist/backward.py
@@ -42,6 +42,10 @@ def backward(mnist):
         init_op = tf.global_variables_initializer()
         sess.ren(init_op)
         
+        ckpt = tf.train.get_checkpoint_state(MODEL_SAVE_PATH) 
+        if ckpt and ckpt.model_checkpoint_path: 
+            saver.restore(sess, ckpt.model_checkpoint_path) 
+            
         for i in range(STEPS):
             xs,ys = mnist.train.next_batch(BATCH_SIZE)
             _,loss_value,step = sess.run([train_op,loss,global_step].feed_dict={x:xs,y:ys})
