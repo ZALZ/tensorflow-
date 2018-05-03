6d4748ccdf9c4218ae32c1ec18109ca5845f16b4
ZALZ <824266572@qq.com>
2018-05-03 19:31:02 +0800

    Update test.py

--- mnist/test.py
+++ mnist/test.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 import time
 import tensorflow as tf
-form tensorflow.example.tutorails.mnist import input_data
+from tensorflow.examples.tutorials.mnist import input_data
 import forward
 import backward
 TEST_INTERVAL_SECS = 5
@@ -10,15 +10,15 @@ def test(mnist):
     with tf.Graph().as_default() as g:
         x = tf.placeholder(tf.float32,[None,forward.INPUT_NODE])
         y_ = tf.placeholder(tf.float32,[None,forward.OUTPUT_NODE])
-        y = forward.forward(x,Node)
-        
+        y = forward.forward(x,None)
+
         ema = tf.train.ExponentialMovingAverage(backward.MOVING_AVERAGE_DECAY)
         ema_restore = ema.variables_to_restore()
         saver = tf.train.Saver(ema_restore)
-        
+
         correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
-        
+
         while True:
             with tf.Session() as sess:
                 ckpt = tf.train.get_checkpoint_state(backward.MODEL_SAVE_PATH)
@@ -30,11 +30,11 @@ def test(mnist):
                 else:
                     print('No checkpoint file found')
                     return
-            timesleep(TEST_INTERVAL_SECS)
-            
+            time.sleep(TEST_INTERVAL_SECS)
+
 def main():
-    mnist = input_data.read_data_sets("./data/",onehot=Ture)
-    rest(mnist)
-    
+    mnist = input_data.read_data_sets("./data/",one_hot=True)
+    test(mnist)
+
 if __name__ == '__main__':
     main()
